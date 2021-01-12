47e9ab904258ed6d7a54643d55657f057abe84cf
Darren Li <dxl@enablehr.com>
2021-01-12 17:38:15 +1100

    feat: add mdx, gatsby-source-filesystem

---
+++ src/pages/contact.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => (
+    <h1 style={{ color: 'purple' }}>Contact</h1>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => (
+    <h1 style={{ color: 'purple' }}>Hello Gatsby</h1>
+)
