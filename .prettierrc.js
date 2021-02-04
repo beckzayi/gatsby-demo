8fc11268788a5bccb7172ad7cf0f0eb66b9b998a
beckzayi <beckzayi@126.com>
2021-02-04 14:28:28 +1100

    feat: code standard, prettier, eslint, husky, lint-staged etc

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    proseWrap: 'preserve',
+    singleQuote: true,
+    trailingComma: 'es5',
+    printWidth: 80,
+    semi: true,
+    jsxBracketSameLine: true,
+    tabWidth: 4,
+};
