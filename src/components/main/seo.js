9e52bda2fb5e2d65dc3066f8f154fe0fa1a7f858
beckzayi <beckzayi@126.com>
2021-03-22 01:20:46 +1100

    feat: seo, title, propTypes

--- src/components/main/seo.js
+++ src/components/main/seo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { useLocation } from '@reach/router';
 import useSiteMetadata from '../../hooks/use-site-metadata';
@@ -7,9 +8,11 @@ const SEO = (props) => {
     const { href, origin } = useLocation();
     const metaData = useSiteMetadata();
     const title = [
-        props.pageContext.identifier || props.pageContext.frontmatter.title || '',
+        props.pageContext.identifier || (props.pageContext.frontmatter && props.pageContext.frontmatter.title) || '',
         props.title || metaData.defaultTitle,
-    ].join(' - ');
+    ]
+        .filter((item) => !item === false) // Remove null, undefined, and empty string
+        .join(' - ');
     const seo = {
         title,
         description: props.description || metaData.defaultDescription,
@@ -17,8 +20,9 @@ const SEO = (props) => {
         image: `${origin}${props.image || metaData.defaultImage}`,
     };
     return (
-        <Helmet title={seo.title}>
-            <meta name="description" content={seo.description} />
+        <Helmet title={seo.title} htmlAttributes={{ lang: 'en' }}>
+            {seo.description && <meta name="description" content={seo.description} />}
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             {seo.url && <meta property="og:url" content={seo.url} />}
             {seo.title && <meta property="og:title" content={seo.title} />}
             {seo.description && <meta property="og:description" content={seo.description} />}
@@ -31,3 +35,15 @@ const SEO = (props) => {
 };
 
 export default SEO;
+
+SEO.propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.string,
+};
+
+SEO.defaultProps = {
+    title: null,
+    description: null,
+    image: null,
+};
